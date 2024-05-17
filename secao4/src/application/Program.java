2d1f8fa9a95b1a4e453a03870e7721a40e629d53
Marcos Guerreiro <mgr@icomp.ufam.edu.br>
2024-05-17 10:44:02 -0400

    Exercício de Enums e Composição concluído parte 2

--- secao4/src/application/Program.java
+++ secao4/src/application/Program.java
@@ -1,8 +1,64 @@
 package application;
 
+import entities.Department;
+import entities.HourContract;
+import entities.Worker;
+import entities.enums.WorkLevel;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Date;
+
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY");
 
+        System.out.print("Enter departament's name: ");
+        String departamentName = sc.nextLine();
+
+        System.out.println("Enter work data: ");
+        System.out.print("Name: ");
+        String workerName = sc.nextLine();
+
+        System.out.print("Level: ");
+        String workerLevel = sc.nextLine();
+
+        System.out.print("Base salary: ");
+        double baseSalary = sc.nextDouble();
+
+        Worker worker = new Worker(workerName, WorkLevel.valueOf(workerLevel), baseSalary, new Department(departamentName));
+
+        System.out.print("How many contracts to this worker? ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n ; i++) {
+            System.out.print("Enter contracts #" + i + " data: ");
+            System.out.print("Date (DD/MM/YYYY): ");
+            Date contracDate = sdf.parse(sc.next());
+
+            System.out.print("Value por hour: ");
+            double valuePerHour = sc.nextDouble();
+
+            System.out.print("Duration (hours): ");
+            int hours = sc.nextInt();
+
+            HourContract contract = new HourContract(contracDate, valuePerHour, hours);
+            worker.addContract(contract);
+        }
+
+        System.out.println();
+        System.out.print("Enter month and year to calculate income (MM/YYYY): ");
+        String monthAndYear = sc.next();
+        int month = Integer.parseInt(monthAndYear.substring(0, 2));
+        int year = Integer.parseInt(monthAndYear.substring(3));
+
+        System.out.println("Name: " + worker.getName());
+        System.out.println("Department: " + worker.getDepartment().getName());
+        System.out.println("Income for: " + monthAndYear + ": " +String.format("%.2f", worker.income(year, month)));
     }
 }
--- secao4/src/entities/enums/WorkLevel.java
+++ secao4/src/entities/enums/WorkLevel.java
@@ -2,13 +2,9 @@ package entities.enums;
 
 public enum WorkLevel {
 
-   JUNIOR(1),
-   MID_LEVEL(2),
-   SENIOR(3);
+   JUNIOR,
+   MID_LEVEL,
+   SENIOR;
 
-   private final int valor;
 
-   WorkLevel(int valorOpcao){
-       valor = valorOpcao;
-   }
 }
