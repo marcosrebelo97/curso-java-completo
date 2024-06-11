a6bbad155152e84b6fd63253ae81e62db5dfe0cc
Marcos Guerreiro <mgr@icomp.ufam.edu.br>
2024-06-10 21:51:49 -0400

    Refatorando código com LocalDateTime

--- secao8/src/services/RentalService.java
+++ secao8/src/services/RentalService.java
@@ -3,6 +3,8 @@ package services;
 import entities.CarRental;
 import entities.Invoice;
 
+import java.time.Duration;
+
 public class RentalService {
 
     private Double pricePerDay;
@@ -18,9 +20,8 @@ public class RentalService {
 
     public void processInvoice(CarRental carRental){
 
-        long t1 = carRental.getStart().getTime();
-        long t2 = carRental.getFinish().getTime();
-        double hours = (double)(t2 - t1) / 100 / 60 / 60;
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        double hours = minutes / 60;
 
         double basicPayment;
         if(hours <= 12){
