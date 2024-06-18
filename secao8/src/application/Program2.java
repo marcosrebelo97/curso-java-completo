f4596d7f46e70a9674d01c3ff01bcef9aa6839aa
Marcos Guerreiro <mgr@icomp.ufam.edu.br>
2024-06-18 19:05:17 -0400

    Iniciando exercício de fixação sobre Interfaces

---
+++ secao8/src/application/Program2.java
@@ -0,0 +1,33 @@
+package application;
+
+import entities.Contract;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program2 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Entre os dados do contrato: ");
+        System.out.print("Numero: ");
+        int number = sc.nextInt();
+        System.out.print("Data (dd/MM/yyyy): ");
+        LocalDate date = LocalDate.parse(sc.next(), fmt);
+        System.out.print("Valor do contrato: ");
+        double totalValue = sc.nextDouble();
+
+        Contract contract = new Contract(number, date, totalValue);
+
+        System.out.print("Entre com o número de parcelas: ");
+        int n = sc.nextInt();
+
+        sc.close();
+
+    }
+}
