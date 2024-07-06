159ba5762dbea87790ea432239d41c2c07333e8d
Marcos Guerreiro <mgr@icomp.ufam.edu.br>
2024-07-05 23:44:53 -0400

    Nivelamento JPA/Hibernate com Maven

---
+++ secao11/src/main/java/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import domain.Person;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Person p1 = new Person(1, "Marcos", "marcos@gmail.com");
+        System.out.println(p1);
+    }
+}
