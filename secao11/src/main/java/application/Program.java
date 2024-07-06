c237c27196c7ead5bf8a2a0a70c9e2a91da9504c
Marcos Guerreiro <mgr@icomp.ufam.edu.br>
2024-07-05 23:53:57 -0400

    Incluindo mapeamentos

--- secao11/src/main/java/application/Program.java
+++ secao11/src/main/java/application/Program.java
@@ -2,10 +2,25 @@ package application;
 
 import domain.Person;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Program {
     public static void main(String[] args) {
 
-        Person p1 = new Person(1, "Marcos", "marcos@gmail.com");
-        System.out.println(p1);
+        Person p1 = new Person(null, "Marcos", "marcos@gmail.com");
+        Person p2 = new Person(null, "Rita", "rita@gmail.com");
+        Person p3 = new Person(null, "Enrico", "enrico@gmail.com");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction();
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+        em.getTransaction().commit();
+
     }
 }
