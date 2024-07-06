12c9a0ed22600eee1c8ce791fbaa1263c78df014
Marcos Guerreiro <mgr@icomp.ufam.edu.br>
2024-07-06 11:26:47 -0400

    Commit inicial projeto web services com spring boot

---
+++ secao12/src/main/java/com/webservices/secao12/Secao12Application.java
@@ -0,0 +1,13 @@
+package com.webservices.secao12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Secao12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Secao12Application.class, args);
+	}
+
+}
---
+++ secao12/src/test/java/com/webservices/secao12/Secao12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservices.secao12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Secao12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
