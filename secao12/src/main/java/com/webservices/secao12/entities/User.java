657412587e90a0b754087b4584b454bf203eb275
Marcos Guerreiro <mgr@icomp.ufam.edu.br>
2024-07-06 14:58:56 -0400

    H2 database, JPA e teste de perfil

--- secao12/src/main/java/com/webservices/secao12/entities/User.java
+++ secao12/src/main/java/com/webservices/secao12/entities/User.java
@@ -1,12 +1,8 @@
 package com.webservices.secao12.entities;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
+import jakarta.persistence.*;
+import lombok.*;
 
-import java.io.Serial;
 import java.io.Serializable;
 
 @Getter
@@ -14,9 +10,13 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
