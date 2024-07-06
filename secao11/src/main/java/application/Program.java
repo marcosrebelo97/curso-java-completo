8450791d376da1397ac0ec4bf93285c1a269efa2
Marcos Guerreiro <mgr@icomp.ufam.edu.br>
2024-07-06 00:01:46 -0400

    Utilizando método buscar do mapeamento.

--- secao11/src/main/java/application/Program.java
+++ secao11/src/main/java/application/Program.java
@@ -9,18 +9,15 @@ import javax.persistence.Persistence;
 public class Program {
     public static void main(String[] args) {
 
-        Person p1 = new Person(null, "Marcos", "marcos@gmail.com");
-        Person p2 = new Person(null, "Rita", "rita@gmail.com");
-        Person p3 = new Person(null, "Enrico", "enrico@gmail.com");
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
 
-        em.getTransaction();
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
-        em.getTransaction().commit();
+        //search for person
+        Person p = em.find(Person.class, 1);
+        System.out.println(p);
+
+        em.close();
+        emf.close();
 
     }
 }
