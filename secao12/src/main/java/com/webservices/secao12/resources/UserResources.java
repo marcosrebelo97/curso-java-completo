4e9a66fbd84434692c3830edd4c903e27040506c
Marcos Guerreiro <mgr@icomp.ufam.edu.br>
2024-07-23 18:56:45 -0400

    Excluindo Usuário

--- secao12/src/main/java/com/webservices/secao12/resources/UserResources.java
+++ secao12/src/main/java/com/webservices/secao12/resources/UserResources.java
@@ -38,4 +38,10 @@ public class UserResources {
         return ResponseEntity.created(uri).body(user);
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
