cc5ab34326706354650d3f69bd2963efd6e2672b
Marcos Guerreiro <mgr@icomp.ufam.edu.br>
2024-07-22 22:11:32 -0400

    Anotação JsonIgnore p/ funcionar métodos SubTotal e Total

--- secao12/src/main/java/com/webservices/secao12/entities/Payment.java
+++ secao12/src/main/java/com/webservices/secao12/entities/Payment.java
@@ -1,6 +1,7 @@
 package com.webservices.secao12.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -25,6 +26,7 @@ public class Payment implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
